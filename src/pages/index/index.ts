d336d857e9259df261140fb72cba27170bb147c2
NewFuture <github@newfuture.cc>
2019-02-14 20:20:22 +0800

    style & ts

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-import { MyApp, } from "../../app";
+import { MyApp } from "../../app";
 
 const app = getApp<MyApp>();
 
@@ -26,7 +26,7 @@ Page({
         } else if (this.data.canIUse) {
             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
             // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
+            app.userInfoReadyCallback = (res) => {
                 this.setData!({
                     userInfo: res,
                     hasUserInfo: true,
@@ -35,7 +35,7 @@ Page({
         } else {
             // 在没有 open-type=getUserInfo 版本的兼容处理
             wx.getUserInfo({
-                success: res => {
+                success: (res) => {
                     app.globalData.userInfo = res.userInfo;
                     this.setData!({
                         userInfo: res.userInfo,
