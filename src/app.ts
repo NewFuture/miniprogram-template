7b7a919edea3a7fd7b29c336f26654304e95a664
New Future <NewFuture@users.noreply.github.com>
2019-08-18 22:46:25 +0800

    Fix eslint (#32)
    * chore(packages): update
    
    * fix(eslint): update config
    
    * feat(eslint): wxs config

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,6 @@ App({
         // 获取用户信息
         wx.getSetting({
             success: (res) => {
-                //@ts-ignore
                 if (res.authSetting["scope.userInfo"]) {
                     // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
                 }
